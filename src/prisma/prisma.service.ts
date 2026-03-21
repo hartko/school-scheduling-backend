21a0841d7b0a510fe6b5355c689c7430089b4ee9
Harriette Rodriguez <harriettemae@captivatechat.com>
2026-03-22 04:07:25 +0800

    fix: db

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,18 @@
 // src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg'; // import the adapter class
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 import PrismaPaginate from 'prisma-paginate';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    const adapter = new PrismaPg({
+    const pool = new Pool({
       connectionString: process.env.DATABASE_URL as string,
       ssl: { rejectUnauthorized: false },
     });
+    const adapter = new PrismaPg(pool);
 
     super({
       adapter, // pass the instantiated adapter
